ed4c40e81b9622430e7e7a09d9fd9285920aaef0
Lee, Randy <rlee@alltechconsulting.com>
2014-09-26 16:09:55 +0000

    pre-import delete
    git-svn-id: https://svn.dot.state.fl.us/svn/DesignQuantityEstimates/trunk@22 75b8a96b-f9f1-ff4d-8c6d-4caf77bb4aec

--- Dqe.ApplicationServices/ITaskRunner.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Dqe.ApplicationServices
-{
-    public interface ITaskRunner
-    {
-        void CopyMasterFile(string taskUser, int masterFileId, DateTime effectiveDate, int currentUserSrsId, int fileNumber);
-    }
-}
--- Dqe.Web/Scripts/app/controllers/homeSelectionProposalController.js
+++
@@ -1,3 +0,0 @@
-﻿dqeControllers.controller('HomeSelectionProposalController', ['$scope', '$rootScope', function ($scope, $rootScope) {
-    $rootScope.$broadcast('initializeNavigation');
-}]);
