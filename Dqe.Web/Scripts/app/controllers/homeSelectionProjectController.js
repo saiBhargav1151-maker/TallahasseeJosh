32229653117bc706fcf1a5f82c5921ac2f974ea0
Marcus Barnes <Marcus.Barnes@dot.state.fl.us>
2025-10-13 14:59:30 -0400

    little fix

--- Dqe.Web/Scripts/app/controllers/homeSelectionProjectController.js
+++ Dqe.Web/Scripts/app/controllers/homeSelectionProjectController.js
@@ -13,7 +13,11 @@
             $scope.hasReviewsInProject = false;
 
             //Takes the char field of QTY_CMPLT_CD in LRE table ('Y' or 'N' which has defalut 'N')
-            $scope.prefferedApplication = r.project.quantityComplete == null ? "Project not in LRE" : r.project.quantityComplete.toUpperCase() === 'Y' ? "DQE" : r.project.quantityComplete.toUpperCase() === 'N' ? "LRE" : "Project not in LRE";
+
+            if ($scope.prefferedApplication === null || $scope.prefferedApplication === undefined) {
+                $scope.prefferedApplication = r.project.quantityComplete == null ? "Project not in LRE" : r.project.quantityComplete.toUpperCase() === 'Y' ? "DQE" : r.project.quantityComplete.toUpperCase() === 'N' ? "LRE" : "Project not in LRE";
+            }
+           
 
             ///checking the first estimate of every version to see if it is a review, if so marking a bool field as true.
             for (var i = 0; i < $scope.versions.length; i++) {
@@ -258,6 +262,7 @@
     $scope.snapshotWorkingEstimate = function (project) {
 
         $http.post('./projectproposal/SnapshotWorkingEstimate', project).success(function (result) {
+            $scope.prefferedApplication = "";
             if (project.quantityComplete.toUpperCase() === 'N' && project.takeLabeledSnapshot != null && project.takeLabeledSnapshot) {
                 result.data.project.quantityComplete = 'Y';
             }
